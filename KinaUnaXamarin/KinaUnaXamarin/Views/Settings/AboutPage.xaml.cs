2f6ceaf06a54f1d013d6c202b45c06eda45f6396
Per Rosing Mogensen <per.mogensen@live.com>
2020-03-15 10:25:27 +0100

    Updated activity indicator.

--- KinaUnaXamarin/KinaUnaXamarin/Views/Settings/AboutPage.xaml.cs
+++ KinaUnaXamarin/KinaUnaXamarin/Views/Settings/AboutPage.xaml.cs
@@ -45,5 +45,25 @@ namespace KinaUnaXamarin.Views.Settings
             SelectProgenyPage selProPage = new SelectProgenyPage(progenyCollection);
             await Shell.Current.Navigation.PushModalAsync(selProPage);
         }
+
+        private async void WebsiteLinkLabel_Tapped(object sender, EventArgs e)
+        {
+            await Launcher.OpenAsync("https://www.kinauna.com");
+        }
+
+        private async void SupportLinkLabel_Tapped(object sender, EventArgs e)
+        {
+            await Launcher.OpenAsync("https://support.kinauna.com");
+        }
+
+        private async void SupportEmailLabel_Tapped(object sender, EventArgs e)
+        {
+            await Launcher.OpenAsync("mailto:support@kinauna.com");
+        }
+
+        private async void SourceCodeLinkLabel_Tapped(object sender, EventArgs e)
+        {
+            await Launcher.OpenAsync("https://github.com/KinaUna/KinaUnaXamarin");
+        }
     }
 }
